fddce2d6c6826394aafcde248e99936adf6be1f8
eduardoalmeidabr <80225178+eduardoalmeidabr@users.noreply.github.com>
2021-03-11 14:03:46 -0300

    Update saude_caixa.py

--- saude_caixa.py
+++ saude_caixa.py
@@ -23,6 +23,7 @@ for i in df.index:
     sexo = df['sexo'][i]
     idade = h.idade(df['data_nascimento'][i], p.data_base)
     tempo_servico = h.tempo_servico(df['data_admissao'][i], p.data_base)
+    tempo_servico_fixo = tempo_servico
     tipo_dependente = df['tipo_dependente'][i]
     relacao_dependencia = df['relacao_dependencia'][i]
     condicao_participante = df['condicao_participante'][i]
@@ -35,6 +36,8 @@ for i in df.index:
     # Valores do primeiro período
     elegibilidade = h.elegibilidade(idade, idade_aposentadoria)
     auxiliar_elegibilidade = h.auxiliar_elegibilidade(idade, idade_aposentadoria)
+    elegibilidade_anterior = h.elegibilidade(idade - 1, idade_aposentadoria)
+    auxiliar_elegibilidade_anterior = h.auxiliar_elegibilidade(idade - 1, idade_aposentadoria)
     rateio = 1.0
     crescimento_folha = h.crescimento_folha(p.taxa_crescimento_folha)
 
@@ -44,19 +47,30 @@ for i in df.index:
     custo_posicionado_conjuge = crescimento_folha * (p.custo_medio * pow(1 + p.aging_factor, \
         idade_conjuge - p.idade_media))
     custo_posicionado_temp = crescimento_folha * (p.custo_medio * (pow(1 + p.aging_factor, \
-        idade_dependente_temporario - idade_media)))
+        idade_dependente_temporario - p.idade_media)))
 
     mensalizacao = h.mensalizacao(idade, sexo)
     mensalizacao_conjuge = h.mensalizacao(idade_conjuge, 'F' if sexo == 'M' else 'M')
 
     qx = h.qx(idade, sexo)
     qy = h.qx(idade, 'F' if sexo == 'M' else 'M')
+    qy_br = h.qx(idade, 'F' if sexo == 'M' else 'M')
+    qz = h.qx(idade_dependente_temporario, sexo)
     ix = h.ix(elegibilidade, idade, pdve_temporario)
     wx = h.wx(elegibilidade, idade, pdve_temporario)
     qxi = h.qxi(idade)
-    tpx = h.tpx(idade, 0)
+
+    qx_anterior = h.qx(idade - 1, sexo)
+    qy_anterior = h.qx(idade - 1, 'F' if sexo == 'M' else 'M')
+    qz_anterior = h.qx(idade_dependente_temporario - 1, sexo)
+    ix_anterior = h.ix(elegibilidade, idade - 1, pdve_temporario)
+    wx_anterior = h.wx(elegibilidade, idade - 1, pdve_temporario)
+    qxi_anterior = h.qxi(idade - 1)
+
+    tpx = 1.0
     tpx_aux_conj = 1.0
     tpy = 1.0
+    tpz = 1.0
     tpix = 1.0
     tpwx = 1.0
     tpxaa = 1.0
@@ -65,51 +79,132 @@ for i in df.index:
     tpyaa_aposentadoria = 1.0
     vx = h.vx(0, p.taxa_desconto)
     perc_casados = h.perc_casados(idade_aposentadoria, sexo)
-    
+
     # Beneficios
+    beneficio_invalidez_titular = 0.0 if elegibilidade else custo_posicionado_titular
+    beneficio_invalidez_conjuge = 0.0 if elegibilidade else custo_posicionado_conjuge
+
+    # Funções auxiliares
+    auxiliar_invalidez_titular = tpxaa_aposentadoria * beneficio_invalidez_titular * \
+        rateio * ix
+    auxiliar_invalidez_conjuge = tpxaa_aposentadoria * beneficio_invalidez_conjuge * \
+        ix * rateio
+    auxiliar_pensao_morte_ativo = tpxaa * beneficio_invalidez_conjuge * \
+        rateio * qx
+    auxiliar_reversao_invalidez_temp = custo_posicionado_temp * tpxaa_aposentadoria * \
+        rateio * ix
+    auxiliar_pensao_morte_temp = tpxaa * custo_posicionado_temp * rateio * qx
+
+    # Passivos
     passivo_titular = custo_posicionado_titular * tpx * tpix * tpwx * rateio * \
         vx * p.fator_capacidade * mensalizacao if elegibilidade else 0   
     passivo_conjuge = custo_posicionado_conjuge * perc_casados * tpx_aux_conj * \
         tpix * tpwx * tpy * p.fator_capacidade * rateio * vx * mensalizacao_conjuge if elegibilidade else 0
+    passivo_aposentadoria_invalidez = auxiliar_invalidez_titular * vx * p.fator_capacidade * mensalizacao
+    passivo_reversao_invalidez = auxiliar_invalidez_conjuge * perc_casados * vx * \
+        p.fator_capacidade * mensalizacao
+    passivo_pensao_morte = auxiliar_pensao_morte_ativo * perc_casados * vx * p.fator_capacidade * \
+        mensalizacao
+    passivo_programado_temporario = custo_posicionado_temp * tpx_aux_conj * tpix * \
+        tpwx * vx * rateio * p.fator_capacidade * perc_casados * mensalizacao_conjuge
+    passivo_reversao_invalidez_temp = auxiliar_reversao_invalidez_temp * perc_casados * \
+        vx * p.fator_capacidade * mensalizacao
+    passivo_pensao_morte_temp = auxiliar_pensao_morte_temp * perc_casados * vx * \
+        p.fator_capacidade * mensalizacao
 
-    print(carteira, tpx, passivo_conjuge, passivo_titular)
-    """
     # Valores a partir do segundo período
     for j in range(idade + 1, 122):
-        
+        # Totais dos passivos
+        total_programado_titular += passivo_titular
+        total_programado_conjuge += passivo_conjuge
+        total_invalidez += passivo_aposentadoria_invalidez
+        total_reversao_invalidez += passivo_reversao_invalidez
+        total_pensao_morte += passivo_pensao_morte
+        total_programado_temp += passivo_programado_temporario
+        total_reversao_invalidez_temp += passivo_reversao_invalidez_temp
+        total_pensao_morte_temp += passivo_pensao_morte_temp
+
+        # Valores do primeiro período
         idade_conjuge += 1
+        idade_dependente_temporario += 1
+        idade_conjuge_br = h.idade_conjuge_br(j, sexo)
         elegibilidade = h.elegibilidade(j, idade_aposentadoria)
         auxiliar_elegibilidade = h.auxiliar_elegibilidade(j, idade_aposentadoria)
+        elegibilidade_anterior = h.elegibilidade(j - 1, idade_aposentadoria)
+        auxiliar_elegibilidade_anterior = h.auxiliar_elegibilidade(j - 1, idade_aposentadoria)
+        rateio = tempo_servico_fixo / tempo_servico
+        crescimento_folha *= h.crescimento_folha(p.taxa_crescimento_folha)
+
         qx = h.qx(j, sexo)
-        qy = h.qx(idade_conjuge, sexo)
-        ix = h.ix(elegibilidade, j, pdve_temporario)
-        wx = h.wx(elegibilidade, j, pdve_temporario)
+        qy = h.qx(j, 'F' if sexo == 'M' else 'M')
+        qy_br = h.qx(j, 'F' if sexo == 'M' else 'M')
+        qz = h.qx(idade_dependente_temporario, sexo)
+        ix = h.ix(elegibilidade, idade, pdve_temporario)
+        wx = h.wx(elegibilidade, idade, pdve_temporario)
         qxi = h.qxi(idade)
 
-        # Funções biométricas do período anterior
         qx_anterior = h.qx(j - 1, sexo)
-        qy_anterior = h.qx(idade_conjuge - 1, sexo)
+        qy_anterior = h.qx(j - 1, 'F' if sexo == 'M' else 'M')
+        qz_anterior = h.qx(idade_dependente_temporario - 1, sexo)
         ix_anterior = h.ix(elegibilidade, j - 1, pdve_temporario)
         wx_anterior = h.wx(elegibilidade, j - 1, pdve_temporario)
         qxi_anterior = h.qxi(j - 1)
-        # As seguintes funções usam as variáveis biométricas do período anterior
+    
         tpx *= (1 - qx_anterior)
-        tpx_aux_conj = tpx if elegibilidade and auxiliar_elegibilidade else tpx_aux_conj * (1 - qx)
-        if (elegibilidade and auxiliar_elegibilidade) or \
-            (not elegibilidade and not auxiliar_elegibilidade):
-            tpy = 1.0
-        else:
-            tpy *= (1 - qy_anterior)
-
+        tpx_aux_conj = tpx_aux_conj if elegibilidade or not auxiliar_elegibilidade else tpx_aux_conj * (1 - qx_anterior)
+        tpy *= (1 - qy_anterior)
+        tpz = 1.0 if (elegibilidade and auxiliar_elegibilidade) or \
+            (not elegibilidade and not auxiliar_elegibilidade) else tpz * (1 - qz_anterior)
         tpix *= (1 - ix_anterior)
         tpwx *= (1 - wx_anterior)
-        tpxaa = 
+        tpxaa = tpxaa * (1 - qx_anterior) if elegibilidade_anterior else tpxaa * (1 - (qx_anterior + ix_anterior + wx_anterior))
+        tpxaa_aposentadoria = tpxaa_aposentadoria if elegibilidade_anterior else tpxaa
+        # TODO Verificar o qy_br_anterior como calcula
+        tpyaa = tpyaa * (1 - qy_br) if elegibilidade_anterior else tpyaa * (1 - (qy_br + ix_anterior + wx_anterior))
+        tpyaa_aposentadoria = tpyaa_aposentadoria if elegibilidade_anterior else tpyaa
         vx = h.vx(j - idade, p.taxa_desconto)
-    """
+        tempo_servico += 1
 
-        
+        # Custos posicionados
+        custo_posicionado_titular = crescimento_folha * (p.custo_medio * pow(1 + p.aging_factor, \
+            j - p.idade_media))
+        custo_posicionado_conjuge = crescimento_folha * (p.custo_medio * pow(1 + p.aging_factor, \
+            idade_conjuge - p.idade_media))
+        custo_posicionado_temp = crescimento_folha * (p.custo_medio * (pow(1 + p.aging_factor, \
+            idade_dependente_temporario - p.idade_media)))
 
+        # Beneficios
+        beneficio_invalidez_titular = 0.0 if elegibilidade else custo_posicionado_titular
+        beneficio_invalidez_conjuge = 0.0 if elegibilidade else custo_posicionado_conjuge
 
+        # Funções auxiliares
+        # TODO Atualizar as fórmulas das funções auxiliares
+        auxiliar_invalidez_titular = tpxaa_aposentadoria * beneficio_invalidez_titular * \
+            rateio * ix
+        auxiliar_invalidez_conjuge = tpxaa_aposentadoria * beneficio_invalidez_conjuge * \
+            ix * rateio
+        auxiliar_pensao_morte_ativo = tpxaa * beneficio_invalidez_conjuge * \
+            rateio * qx
+        auxiliar_reversao_invalidez_temp = custo_posicionado_temp * tpxaa_aposentadoria * \
+            rateio * ix
+        auxiliar_pensao_morte_temp = tpxaa * custo_posicionado_temp * rateio * qx
+
+        # Passivos
+        passivo_titular = custo_posicionado_titular * tpx * tpix * tpwx * rateio * \
+            vx * p.fator_capacidade * mensalizacao if elegibilidade else 0
+        passivo_conjuge = custo_posicionado_conjuge * perc_casados * tpx_aux_conj * \
+            tpix * tpwx * tpy * p.fator_capacidade * rateio * vx * mensalizacao_conjuge if elegibilidade else 0
+        passivo_aposentadoria_invalidez = auxiliar_invalidez_titular * vx * p.fator_capacidade * mensalizacao
+        passivo_reversao_invalidez = auxiliar_invalidez_conjuge * perc_casados * vx * \
+            p.fator_capacidade * mensalizacao
+        passivo_pensao_morte = auxiliar_pensao_morte_ativo * perc_casados * vx * p.fator_capacidade * \
+            mensalizacao
+        passivo_programado_temporario = custo_posicionado_temp * tpx_aux_conj * tpix * \
+            tpwx * vx * rateio * p.fator_capacidade * perc_casados * mensalizacao_conjuge
+        passivo_reversao_invalidez_temp = auxiliar_reversao_invalidez_temp * perc_casados * \
+            vx * p.fator_capacidade * mensalizacao
+        passivo_pensao_morte_temp = auxiliar_pensao_morte_temp * perc_casados * vx * \
+            p.fator_capacidade * mensalizacao
 
 
 
